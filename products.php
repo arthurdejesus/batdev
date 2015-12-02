095277ab0544e44620ee51baf5f588e4ca7c8e12
ninasalvador <philwebservices.pwsojt5@gmail.com>
2015-12-02 18:41:12 +0800

    Update

--- products.php
+++ products.php
@@ -64,41 +64,27 @@
     </section>
 </div>
 
-    <section class="product-section animatedParent animateOnce" data-section-name="trampoline-parks">
-        <div class="grid-we-do-item p-0">
-            <div class="grid-we-do-img-wrap grid-wrap-black">
-                <div class="row">
-                    <div class="col-md-3 p-0">
-                        <div class="grid-product-type-blue">
-                            <h1 class="grid-product-title">Sky Park</h1>
-                        </div>
-                    </div>
-                    <div class="col-md-9 p-0">
-                        <div class="grid-product-wrap-blue" style="background-image:url('images/products/p3.p
-   <div class="section-warning animatedParent animateOnce" style="background-image: url('images/bg-warning.jpg');">
-                        <div class="wa
-deInLeft slow">Warning<img src="images/warning-icon.png"></h3>
-                        </div>
-                        <div class="warning       <div class="content animated fadeght slow">
-                        <div class="destion">
-                            <p>Landing on the head or neck can cause serious injury, paralysis,
-                                death, even when landing i <p>Use trampe only with mature, knowledgeable supervision.  </p>
-                        </div>
-                    </div>
-                    iv>
-                </div>
-                or neck can cause serious injury, paralysis,
-                or death, even when landing in the middle of the bed.</p>
+    <div class="section-warning animatedParent animateOnce" style="background-image: url('images/bg-warning.jpg');">
+        <div class="warning-wrap">
+            <div class="warning-grid mw-410">
+                <h3 class="warning-title animated fadeInLeft slow">Warning<img src="images/warning-icon.png"></h3>
             </div>
-        </div>
-        </div>
-        <div class="warning-grid">
-            <div class="content animated fadeInRight slow">
-                <div class="description">
-                    <p>Use trampoline only with mature, knowledgeable supervision.  </p>
+            <div class="warning-grid">
+                <div class="content animated fadeInRight slow">
+                    <div class="description">
+                        <p>Landing on the head or neck can cause serious injury, paralysis,
+                            or death, even when landing in the middle of the bed.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="warning-grid">
+                <div class="content animated fadeInRight slow">
+                    <div class="description">
+                        <p>Use trampoline only with mature, knowledgeable supervision.  </p>
+                    </div>
+
                 </div>
             </div>
         </div>
-        </div>
-        </section>
+    </div>
 <?php include 'footer.php'; ?>
