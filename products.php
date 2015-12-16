e1d18d558c8d7c6fa8ddf983d3c9c0edf916daa7
ninasalvador <philwebservices.pwsojt5@gmail.com>
2015-12-16 17:29:41 +0800

    Update

--- products.php
+++ products.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'; ?>
 <div class="product-wrap">
-    <div class="product-list-item" style="background-color: #921714;">
+    <div class="product-list-item" style="background-color: #982926;">
         <div class="product-list-item-bg" style="background-image: url('images/product-page/Ninja-Course-2.jpg');"></div>
         <div class="container product-list-item-container">
             <div class="product-list-item-content product-list-item-img-wrap">
@@ -26,12 +26,235 @@
         </div>
     </div>
 
-    <div class="product-list-item product-list-item-right" style="background-color: #1A824E;">
+    <div class="product-list-item product-list-item-right" style="background-color: #2D2A2A;">
         <div class="product-list-item-bg" style="background-image: url('images/product-page/Aeroball-1.jpg');"></div>
         <div class="container product-list-item-container">
             <div class="product-list-item-content product-list-item-desc-wrap">
                 <div class="content">
                     <h3 class="product-title">AeroBall</h3>
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+
+            <div class="product-list-item-content product-list-item-img-wrap">
+                <div class="product-list-item-img-big">
+                    <div class="product-img" style="background-image: url('images/product-page/Aeroball-1.jpg');
+                    "></div>
+
+                    <div class="product-list-item-img-small">
+                        <div class="product-img" style="background-image: url('images/product-page/Aeroball-2.jpg');
+                        "></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="product-list-item" style="background-color: #1A824E;">
+        <div class="product-list-item-bg" style="background-image: url('images/product-page/Airbag-1.jpg');"></div>
+        <div class="container product-list-item-container">
+            <div class="product-list-item-content product-list-item-img-wrap">
+                <div class="product-list-item-img-big">
+                    <div class="product-img" style="background-image: url('images/product-page/Airbag-1.jpg');
+                    "></div>
+
+                    <div class="product-list-item-img-small">
+                        <div class="product-img" style="background-image: url('images/product-page/Airbag-2.jpg');
+                        "></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-list-item-content product-list-item-desc-wrap">
+                <div class="content">
+                    <h3 class="product-title">Airbag Pit</h3>
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="product-list-item product-list-item-right" style="background-color: #354980;">
+        <div class="product-list-item-bg" style="background-image: url('images/product-page/Climbing-1.jpg');"></div>
+        <div class="container product-list-item-container">
+            <div class="product-list-item-content product-list-item-desc-wrap">
+                <div class="content">
+                    <h3 class="product-title">Climbing Wall/Climbing Climbing Tower</h3>
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+
+            <div class="product-list-item-content product-list-item-img-wrap">
+                <div class="product-list-item-img-big">
+                    <div class="product-img" style="background-image: url('images/product-page/Climbing-1.jpg');
+                    "></div>
+
+                    <div class="product-list-item-img-small">
+                        <div class="product-img" style="background-image: url('images/product-page/Climbing-2.jpg');
+                        "></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="product-list-item" style="background-color: #B95E2D;">
+        <div class="product-list-item-bg" style="background-image: url('images/product-page/Dodgeball-1.jpg');"></div>
+        <div class="container product-list-item-container">
+            <div class="product-list-item-content product-list-item-img-wrap">
+                <div class="product-list-item-img-big">
+                    <div class="product-img" style="background-image: url('images/product-page/Dodgeball-1.jpg');
+                    "></div>
+
+                    <div class="product-list-item-img-small">
+                        <div class="product-img" style="background-image: url('images/product-page/Dodgeball-2.jpg');
+                        "></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-list-item-content product-list-item-desc-wrap">
+                <div class="content">
+                    <h3 class="product-title">Dodgeball Trampoline Courts</h3>
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="product-list-item product-list-item-right" style="background-color: #2D2A2A;">
+        <div class="product-list-item-bg" style="background-image: url('images/product-page/main-1.jpg');"></div>
+        <div class="container product-list-item-container">
+            <div class="product-list-item-content product-list-item-desc-wrap">
+                <div class="content">
+                    <h3 class="product-title">Main Trampoline Courts</h3>
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+
+            <div class="product-list-item-content product-list-item-img-wrap">
+                <div class="product-list-item-img-big">
+                    <div class="product-img" style="background-image: url('images/product-page/main-1.jpg');
+                    "></div>
+
+                    <div class="product-list-item-img-small">
+                        <div class="product-img" style="background-image: url('images/product-page/main-2.jpg');
+                        "></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="product-list-item" style="background-color: #982926;">
+        <div class="product-list-item-bg" style="background-image: url('images/product-page/Kid-1.jpg');"></div>
+        <div class="container product-list-item-container">
+            <div class="product-list-item-content product-list-item-img-wrap">
+                <div class="product-list-item-img-big">
+                    <div class="product-img" style="background-image: url('images/product-page/Kid-1.jpg');
+                    "></div>
+
+                    <div class="product-list-item-img-small">
+                        <div class="product-img" style="background-image: url('images/product-page/Kid-2.jpg');
+                        "></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-list-item-content product-list-item-desc-wrap">
+                <div class="content">
+                    <h3 class="product-title">Kid’s Trampoline Courts</h3>
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="product-list-item product-list-item-right" style="background-color: #B95E2D;">
+        <div class="product-list-item-bg" style="background-image: url('images/product-page/Basketball-1.jpg');"></div>
+        <div class="container product-list-item-container">
+            <div class="product-list-item-content product-list-item-desc-wrap">
+                <div class="content">
+                    <h3 class="product-title">Basketball Slam Dunk Trampoline Courts</h3>
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+
+            <div class="product-list-item-content product-list-item-img-wrap">
+                <div class="product-list-item-img-big">
+                    <div class="product-img" style="background-image: url('images/product-page/Basketball-1.jpg');
+                    "></div>
+
+                    <div class="product-list-item-img-small">
+                        <div class="product-img" style="background-image: url('images/product-page/Basketball-2.jpg');
+                        "></div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="product-list-item" style="background-color: #354980;">
+        <div class="product-list-item-bg" style="background-image: url('images/product-page/backyard-1.jpg');"></div>
+        <div class="container product-list-item-container">
+            <div class="product-list-item-content product-list-item-img-wrap">
+                <div class="product-list-item-img-big">
+                    <div class="product-img" style="background-image: url('images/product-page/backyard-1.jpg');
+                    "></div>
+
+                    <div class="product-list-item-img-small">
+                        <div class="product-img" style="background-image: url('images/product-page/backyard-2.jpg');
+                        "></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-list-item-content product-list-item-desc-wrap">
+                <div class="content">
+                    <h3 class="product-title">Backyard Trampolines</h3>
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Learn More</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="product-list-item product-list-item-right" style="background-color: #1A824E;">
+        <div class="product-list-item-bg" style="background-image: url('images/product-page/Aeroball-1.jpg');"></div>
+        <div class="container product-list-item-container">
+            <div class="product-list-item-content product-list-item-desc-wrap">
+                <div class="content">
+                    <h3 class="product-title">Software</h3>
                     <div class="description">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     </div>
